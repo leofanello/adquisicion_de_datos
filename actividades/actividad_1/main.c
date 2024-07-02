26279b008491161dcea0b87abc49646ac4090a93
leofanello <166129610+leofanello@users.noreply.github.com>
2024-07-02 16:44:30 -0300

    Update main.c

--- actividades/actividad_1/main.c
+++ actividades/actividad_1/main.c
@@ -1,52 +1,55 @@
 // Inclusion de bibliotecas
 #include <stdio.h>
-#include <math.h>
 #include "pico/stdlib.h"
-#include "hardware/adc.h"
 #include "hardware/i2c.h"
 #include "lcd_i2c.h"
+#include "bmp280.h"
 
 /*
  * @brief Programa principal
  */ 
 int main() {
-    // Variable para almacenar el resultado del ADC
-    uint16_t adc_value = 0;
-    // Variable para guardar el valor de temperatura
-    float temperatura = 0.0;
-    // Constante de proporcionalidad del termistor
-    const uint16_t beta = 3950;
     // Habilito USB
     stdio_init_all();
-    // Configuro el I2C0 a 100 KHz de clock
-    i2c_init(i2c0, 100 * 1000);
-    // Elijo GPIO4 como linea de SDA
+    // Incicializacion del I2C0
+    i2c_init(i2c0, 100000);
     gpio_set_function(4, GPIO_FUNC_I2C);
-    // Elijo GPIO5 como linea de SCL
     gpio_set_function(5, GPIO_FUNC_I2C);
-    // Activo pull-up en ambos GPIO, son debiles por lo que
-    // es recomendable usar pull-ups externas
     gpio_pull_up(4);
     gpio_pull_up(5);
     // Inicializacion del LCD
     lcd_init();
-    // Inicializo ADC
+    // Inicializo BMP280
+    bmp280_init();
 
-    // Inicializo GPIO26 como entrada analogica
+    // Obtengo parametros de compensacion
+    struct bmp280_calib_param params;
+    bmp280_get_calib_params(&params);
 
-    // Selecciono canal analogico
+    // Variables para temperatura y presion
+    int32_t raw_temperature;
+    int32_t raw_pressure;
+    // Variable para texto 
+    char str[16];
+    // Darle tiempo al BMP que se calibre
+    sleep_ms(250);
 
     while(true) {
-        // Leer NTC
-        
-        // Calculo temperatura
-        
+        // Leo temperatura y presion
+        bmp280_read_raw(&raw_temperature, &raw_pressure);
+        // Calculo temperatura y presion
+        int32_t temperature = bmp280_convert_temp(raw_temperature, &params);
+        int32_t pressure = bmp280_convert_pressure(raw_pressure, raw_temperature, &params);
         // Limpio LCD
         lcd_clear();
-        // Variable para el string
-        char str[16];
-        // Creo string
-        
+        // Armo string
+        sprintf(str, "P=%.3f kPa", pressure / 1000.f);
+        // Imprimo string en primer fila
+        lcd_string(str);
+        // Muevo a segunda fila
+        lcd_set_cursor(1, 0);
+        // Creo segundo string
+        sprintf(str, "T=%.2f C", temperature / 100.f);
         // Imprimo string en segunda fila
         lcd_string(str);
         // Espero 500 ms
